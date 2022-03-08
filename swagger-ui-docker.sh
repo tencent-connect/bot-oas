1cbabaef4796c1065702e0483c83a1fe0eff785e
vissong <vissong@tencent.com>
2022-03-08 13:57:45 +0800

    swagger-ui-docker.sh: update docker cmd

--- swagger-ui-docker.sh
+++ swagger-ui-docker.sh
@@ -2,7 +2,10 @@
 
 docker run -p 8081:8080 \
 -e URLS='[{url: "v1/openapi.yaml", name: "接口定义"},{url: "v1/components.yaml", name: "对象定义"}]' \
--v /Users/vissong/Dev/git/bot-oas/v1/:/usr/share/nginx/html/v1 \
+-e TRY_IT_OUT_ENABLED=true \
+-e PERSIST_AUTHORIZATION=true \
+-e DEEP_LINKING=true \
 -h swagger-ui \
+-v /Users/vissong/Dev/git/bot-oas/v1/:/usr/share/nginx/html/v1 \
 --rm -d swaggerapi/swagger-ui && \
 open http://127.0.0.1:8081
